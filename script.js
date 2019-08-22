1f03f5a4209edb52e8f04f76df2af49d60b406d0
yanalalmer <yanalalmer@yahoo.com>
2019-08-22 12:42:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hadFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.toggle('flip');
+    if(!hadFlippedCard) {
+        hadFlippedCard = true;
+        firstCard = this;
+        return;
+    } 
+        
+        secondCard = this;
+        checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards(): unflipCards()
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard); 
+
+    resetBoard();
+}
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(()=>{
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    
+    resetBoard();
+}, 1000);
+}
+
+function resetBoard() {
+   [hadFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null]; 
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard))
